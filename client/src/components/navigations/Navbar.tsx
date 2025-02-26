2a6683a53f98596a2ced507c8ae582004b0181c7
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-02-26 14:43:11 +0800

    📖 layout: welcome screen guideline

--- client/src/components/navigations/Navbar.tsx
+++ client/src/components/navigations/Navbar.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { Link } from "@tanstack/react-router";
 
 import { Title, Nav, NavLayout, Paragraph } from "@/style/globalStyles";
@@ -18,9 +18,9 @@ const Navbar = () => {
           <LogoImage src={AppLogo} alt="logo" />
           <Title>DNASeq Explorer</Title>
         </NavbarLogo>
-        <div style={{ position: "relative" }}>
+        <div style={{ position: "relative" }} onClick={toggleMenu}>
           <NavMenu>
-            <StyledMenu size={24} onClick={toggleMenu} />
+            {isMenuOpen ? <StyledIcon as={X} size={26} /> : <StyledIcon size={26} />}
           </NavMenu>
           <NavList $isOpen={isMenuOpen}>
             {navitems.map((nav) => {
@@ -29,14 +29,14 @@ const Navbar = () => {
                 <StyledLink key={nav.id} to={nav.link}>
                   <Item>
                     <Icon />
-                    <Paragraph $alignment="left" size="lg">{nav.name}</Paragraph>
-                    </Item>
+                    <Paragraph $alignment="left" size="lg" color="light">{nav.name}</Paragraph>
+                  </Item>
                 </StyledLink>
               )
             })}
-          </NavList>
-        </div>
-      </NavLayout>
+            </NavList>
+          </div>
+        </NavLayout>
     </Nav>
   );
 };
@@ -56,7 +56,7 @@ const NavMenu = styled.div`
   cursor: pointer;
 `;
 
-const StyledMenu = styled(Menu)`
+const StyledIcon = styled(Menu || X)`
   transition: color 0.5s ease-in-out, transform 0.5s;
   &:hover {
     color: ${(props) => props.theme.color.canvas};
@@ -65,18 +65,17 @@ const StyledMenu = styled(Menu)`
 `;
 
 const NavList = styled.div<{ $isOpen: boolean; }>`
-  position: absolute;
+  position: fixed;
   display: flex;
   flex-direction: column;
   gap:12px;
-  right: -20px;
-  top: 60px;
+  right:0;
+  top: 24;
   padding: 14px;
   width: 300px;
   height: 90vh;
   background-color: ${(props) => props.theme.color.accent};
-
-  transform: translateX(${(props) => (props.$isOpen ? "0" : "100%")});
+  transform: translateX(${(props) => (props.$isOpen ? "0" : "calc(100% + 20px)")});
   transition: transform 0.5s ease-in-out;
 `;
 
@@ -109,6 +108,7 @@ const Item = styled.div`
 const StyledLink = styled(Link)`
   color: ${(props) => props.theme.color.light};
   text-decoration: none;
+  padding:8px;
 `
 const LogoImage = styled.img`
   width: 40px;
--- client/src/routes/index.lazy.tsx
+++ client/src/routes/index.lazy.tsx
@@ -1,4 +1,4 @@
-import { Paragraph, Title } from '@/style/globalStyles'
+import { Paragraph, Title, UnorderedList, BulletPoint, Flex } from '@/style/globalStyles'
 import { createLazyFileRoute } from '@tanstack/react-router'
 
 import styled from 'styled-components'
@@ -10,17 +10,39 @@ export const Route = createLazyFileRoute('/')({
 
 function RouteComponent() {
   return (
-    <div className="">
+    <div style={{width:"100%"}}>
       <Title>Simplified Bioinformatics Tool For Sequence Analysis</Title>  
       <ParagraphContainer>
-        <Paragraph $alignment="justify" color="dark">
+        <Paragraph $alignment="justify">
           DNASeq Explorer is a simple web application designed to help life science enthusiasts to explore &
           analyzing biological sequences. Whether you&apos;re a student just starting to learn about molecular biology
           or a researcher looking for quick and seamless tool, this app offers you some features
           assisting on common DNA Analysis tasks.
         </Paragraph>
       </ParagraphContainer>
-    </div>)
+      <Flex>
+        <Block>
+          <Title>How to Get Started?</Title>
+          <UnorderedList>
+            <BulletPoint>
+              You can check the available features on the sidebar
+              which consists of basic analysis, advanced analysis, dot plot alignment,
+              local alignment, and global alignment
+            </BulletPoint>
+            <BulletPoint>
+              Provide an input sequence you want to analyze, for basic & advanced analysis (only one sequence is allowed)
+              the other features are for pairwise sequence alignments using different algorithms.
+            </BulletPoint>
+            <BulletPoint>
+              The results would be displayed accordingly on the output screen provided, and longer sequence may take a longer time
+              to process. Take note that the AI-feature may had provide some mistakes and does not represent every existing gene.
+            </BulletPoint>
+          </UnorderedList>
+        </Block>
+        <Block></Block>
+      </Flex>
+    </div>
+  )
 }
 
 
@@ -28,6 +50,17 @@ const ParagraphContainer = styled.div`
   width: 100%;  
 
   ${mediaQuery("md")} {
-    width: 60%;
+    width: 85%;
+  }
+
+  ${mediaQuery("xl")} {
+    width: 70%;
   }
 `
+
+const Block = styled.div`
+  border: 1px solid #000;
+  width: 550px;
+  height: 450px;  
+`
+
