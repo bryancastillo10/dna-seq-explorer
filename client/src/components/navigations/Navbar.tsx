372738a2c75eac2df6005e3e3b53ef81105e0cb2
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-03-03 13:33:24 +0800

    🧩 component: initial nav bar

--- client/src/components/navigations/Navbar.tsx
+++ client/src/components/navigations/Navbar.tsx
@@ -1,17 +1,42 @@
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { useState } from "react";
+import { AppBar, Container, Typography, IconButton, Box, Toolbar } from "@mui/material";
+import { Menu } from "lucide-react";
+
+import Sidebar from "@/components/navigations/Sidebar";
+import AppLogo from "@/assets/base-dna.png";
 
 export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  
+  const toggleDrawer = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
-    <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          DNA Seq Explorer
-        </Typography>
-        <Button color="inherit">Menu</Button>
-      </Toolbar>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar sx={{height: 20}} disableGutters>
+          <Box
+            component="img"
+            src={AppLogo}
+            alt="App Logo"
+            sx={{ width: 40, height: 50, mr: 2}}
+          />
+          <Typography
+            variant="h3"
+            sx={{ flexGrow: 1, fontFamily: "Poppins" }}>
+            DNA Seq Explorer
+          </Typography>
+          <IconButton color="inherit" onClick={toggleDrawer}>
+            <Menu size={26} />
+          </IconButton>
+          <Sidebar
+            openMenu={openMenu}
+            toggleDrawer={toggleDrawer}
+          />
+        </Toolbar>
+      </Container>
+      
     </AppBar>
   );
 }
---
+++ client/src/constants/theme.ts
@@ -0,0 +1,41 @@
+import { createTheme } from '@mui/material/styles';
+
+
+export const theme = createTheme({
+  // Colors
+  palette: {
+    background: {
+        default: "#FFECD1"    
+    },
+    primary: {
+      light: '#33AB9F',
+      main: '#009688',
+      dark: '#001524',
+      contrastText: '#F4F3F2',
+    },
+    secondary: {
+      light: '#FFE1C6',
+      main: '#FFECD1',
+      dark: '#8D818C',
+      contrastText: '#001524',
+    },
+  },
+
+  // Font Styles, Font Size, Font Weight
+  typography: {
+      fontFamily: ["-apple-system", "Poppins", "sans-serif"].join(','),
+      fontSize: 16,
+      h1: {
+        fontSize: "2rem",
+        fontWeight: 700   
+      },
+      h2: {
+        fontSize: "1.75rem",
+        fontWeight: 600  
+      },
+      h3: {
+        fontSize: "1.55rem",
+        fontWeight: 500
+      }
+    }
+});
