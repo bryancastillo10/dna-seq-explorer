dadb24e308b0220818b20f3b35ab5713877141e5
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-03-10 16:19:06 +0800

    🔐 security: added cors middleware

---
+++ server/utils/cors.py
@@ -0,0 +1,15 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
+def configure_cors(app: FastAPI):
+	origins = [
+		"http://localhost:5173"
+	]
+
+	app.add_middleware(
+		CORSMiddleware,
+		allow_origins=origins,     
+        allow_credentials=True,
+        allow_methods=["POST"],        
+        allow_headers=["*"], 
+	)
