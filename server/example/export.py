7b31774130d79b49b98ede430ed9d9094a080543
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-03-15 10:36:51 +0800

    🖊️ tutorial: reportalb lib pdf tables part 1

--- server/example/export.py
+++ server/example/export.py
@@ -14,13 +14,10 @@ textLines = [
 	'Translation'
 ]
 
-
-
-
-
+# Initial Set Up
 pdf = canvas.Canvas(file_name);
 pdf.setTitle(doc_title);
-drawRuler(pdf)
+drawRuler(pdf) 
 
 # Title
 pdf.setFillColor(HexColor("#009688"))
@@ -41,5 +38,41 @@ text.setFillColor(HexColor("#001524"))
 for line in textLines:
 	text.textLine(line)
 
-pdf.drawText(text)
-pdf.save()
+# Table
+data =[
+	['Dedicated Hosting', 'VPS Hosting', 'Sharing','Domains'],
+	['$200/month','$100/month','$20/month','$50/month'],
+	['Paid Domain','Paid Domain','Free Domain','Free Domain'],
+	['2GB DDR2','20 GB Disk Space','Unlimited Emails','2 GB DDR2']
+]
+
+from reportlab.platypus import SimpleDocTemplate
+
+from reportlab.lib.pagesizes import letter
+
+pdf = SimpleDocTemplate(
+	file_name,
+	pagesize=letter
+)
+
+from reportlab.platypus import Table
+from reportlab.platypus import TableStyle
+from reportlab.lib import colors
+
+table = Table(data)
+style = TableStyle([
+	('BACKGROUND',(0,0),(3,0),colors.green),
+	('TEXTCOLOR',(0,0),(-1,0), colors.whitesmoke),
+	('FONTSIZE',(0,0),(-1,0),14),
+	('BOTTOMPADDING', (0,0),(-1,0),12),
+	('BACKGROUND',(0,1),(-1,-1),colors.beige)
+])
+
+table.setStyle(style)
+
+elements = []
+elements.append(table)
+pdf.build(elements)
+
+
+# pdf.save()
