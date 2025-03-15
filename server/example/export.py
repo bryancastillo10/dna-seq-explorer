cf98a4b8d3150cd46d27d53d77d55df89c40d415
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-03-15 09:23:33 +0800

    🖋️ tutorial: reportlab lib pdf custom format

--- server/example/export.py
+++ server/example/export.py
@@ -1,5 +1,7 @@
 from reportlab.pdfgen import canvas
+from drawRuler import drawRuler
 
+from reportlab.lib.colors import HexColor
 #Contents
 file_name = 'ExampleDoc.pdf'
 doc_title= "The Document Title"
@@ -12,4 +14,32 @@ textLines = [
 	'Translation'
 ]
 
-pdf = canvas.Canvas();
+
+
+
+
+pdf = canvas.Canvas(file_name);
+pdf.setTitle(doc_title);
+drawRuler(pdf)
+
+# Title
+pdf.setFillColor(HexColor("#009688"))
+pdf.setFont('Times-Bold',28)
+pdf.drawCentredString(270,770,title)
+
+pdf.setFont('Times-Roman',18)
+pdf.drawCentredString(290,720,subTitle)
+
+# Line
+pdf.line(30,710, 550, 710)
+
+# Begin a Text Object
+text = pdf.beginText(40,680)
+text.setFont('Times-Roman',16)
+text.setFillColor(HexColor("#001524"))
+
+for line in textLines:
+	text.textLine(line)
+
+pdf.drawText(text)
+pdf.save()
