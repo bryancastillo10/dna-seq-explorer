f6dd8fa9f33ca294cb4197dbc78893d72802b88c
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-03-17 12:32:55 +0800

    📝 pdf: base template header&footer

--- server/lib/pdf_base_template.py
+++ server/lib/pdf_base_template.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from reportlab.lib.colors import HexColor
+from reportlab.lib.pagesizes import letter
 
 def write_canvas_header(canvas, doc, feature_title):
+	""" PDF file header"""
 	doc_title = "DNASeqExplorer Analysis Result"
 	header_title = "DNA Seq Explorer"
 	header_subtitle = "Simplified Tool for Biological Sequence Analysis & Sequencing"
@@ -13,5 +16,16 @@ def write_canvas_header(canvas, doc, feature_title):
 	canvas.drawCentredString(270, 750, header_title)
 	canvas.setFont("Times-Roman", 18)
 	canvas.drawCentredString(290, 700, header_subtitle)
-	canvas.drawCentredString(320,680, feature_title)
+	canvas.drawCentredString(320,670, feature_title)
 	canvas.line(30,660, 550, 660)
+
+def write_canvas_footer(canvas, doc):
+    """PDF file footer"""
+
+    width, height = letter
+    current_date = datetime.now().strftime("%Y-%m-%d")
+
+    footer_text = f"Date: {current_date} | Page {canvas.getPageNumber()}"
+    
+    canvas.setFont("Times-Roman", 10)
+    canvas.drawRightString(width - 30, 30, footer_text)  # Align to the right
--- server/service/file_export/pdf.py
+++ server/service/file_export/pdf.py
@@ -4,7 +4,7 @@ from reportlab.platypus import SimpleDocTemplate, Spacer
 from reportlab.lib.pagesizes import letter
 
 from service.file_export.constants import basic_nuc_mapping
-from lib.write_pdfHeader import write_canvas_header
+from lib.pdf_base_template import write_canvas_header, write_canvas_footer
 
 def export_basic_advanced(feature_title, results:Dict[str,Any], seq_label, save_file):
 		""" Exporting the results of basic or advanced analysis feature """
@@ -12,7 +12,10 @@ def export_basic_advanced(feature_title, results:Dict[str,Any], seq_label, save_
 		elements = []
 		elements.append(Spacer(1, 100))
 
-		doc.build(elements, onFirstPage=lambda c,d: write_canvas_header(c,d, feature_title))
+		doc.build(
+			elements,
+		 	onFirstPage=lambda c,d: (write_canvas_header(c,d, feature_title), write_canvas_footer(c, d)),
+			onLaterPages= write_canvas_footer)
 
 def export_pairwise(feature_title, results:Dict[str,Any], seq_A_label, seq_B_label, save_file):
 		""" Exporting the results of pairwise sequenceing feature """
