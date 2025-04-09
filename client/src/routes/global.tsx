4fa45eaae1e322e31ddccf4c653fce21147d8b0c
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-04-09 15:54:31 +0800

    🌐 feature: save modal for local & global alignment

--- client/src/routes/global.tsx
+++ client/src/routes/global.tsx
@@ -12,6 +12,9 @@ import useGlobalAlignment from '@/features/pairSeq/hooks/useGlobalAlignment';
 import useDelayedLoading from '@/hooks/useDelayedLoading';
 import NullOutput from '@/components/layout/NullOutput';
 
+import { SaveModal } from '@/components/ui/outputs';
+import { useModalStore } from '@/zustand/modal';
+
 export const Route = createFileRoute('/global')({
   component: RouteComponent,
 })
@@ -26,6 +29,8 @@ function RouteComponent() {
   const result = sequencingResult?.data;
 
   const delayedLoading = useDelayedLoading(loading);
+
+  const { isOpen, closeModal } = useModalStore();
 
   return (
     <Stack width="100%">
@@ -45,6 +50,17 @@ function RouteComponent() {
           reset={reset}
       /> : <NullOutput/>)}
       </OutputBlock>
+
+
+      <SaveModal
+        isOpen={isOpen}
+        onClose={closeModal}
+         {...(result ? { 
+                sampleALabel: result.seqALabel,
+                sampleBLabel: result.seqBLabel
+           }: {})}
+      />
+
     </Stack>
   </Stack>
   )
